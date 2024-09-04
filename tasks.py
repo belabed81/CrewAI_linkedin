413eab13031b0464b5959ff9c9fa6d84cca1da5f
belabed81 <43716944+belabed81@users.noreply.github.com>
2024-09-04 11:32:29 +0400

    tasks.py

--- tasks.py
+++ tasks.py
@@ -12,18 +12,18 @@ scrape_linkedin_task = Task(
 
 web_research_task = Task(
     description=dedent(
-        "Get valuable and high quality web information about the comparison between Llama 2 and Llama 3"),
-    expected_output=dedent("Your task is to gather high quality information about the comparison"
-                           " between Llama 2 and Llama 3"),
+        "Get valuable and high quality web information about latest cybersecurity trends"),
+    expected_output=dedent("Your task is to gather high quality information about latest cybersecurity trends"),
+            
     agent=web_researcher_agent,
 )
 
 create_linkedin_post_task = Task(
     description=dedent(
-        "Create a LinkedIn post comparing Llama 2 and Llama 3 following the writing-style "
+        "Create a LinkedIn post about cybersecurity trends following the writing-style "
         "expressed in the scraped LinkedIn posts."
     ),
-    expected_output=dedent("A high-quality and engaging LinkedIn post comparing Llama 2 and Llama 3."
+    expected_output=dedent("A high-quality and engaging LinkedIn post about cybersecurity trends."
                            " The LinkedIn post must follow"
                            " the same writing-style as the one expressed in the scraped LinkedIn posts"),
     agent=doppelganger_agent,
